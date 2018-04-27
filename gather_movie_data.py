fa5caf3ee9abfa78269eca6b5499ba75a4ed0527
jose Morales <jmorales.mv@gmail.com>
2018-04-27 05:38:52 -0500

    data collection complete

--- gather_movie_data.py
+++ gather_movie_data.py
@@ -9,29 +9,18 @@ def store_movie_data(dest):
     bottom_movies = open("./movie_ids_bottom_250.txt", 'r').read()
 
     ia = imdb.IMDb()
-    titles = []
+    summaries = []
     n = 0
-    for line in top_movies.strip().split('\n'):
+    for line in bottom_movies.strip().split('\n'):
         print("Progress: " + str(n) + "%")
         n += 1
         movie = ia.get_movie(line)
-        titles.append(movie['title'])
-        #summaries.append(movie['plot'])
-        #print(movie['title'])
+        summaries.append(str(movie['plot']))
         #print(movie['plot'])
-
-    joined_lines = '\n'.join(titles)
+    joined_lines = '\n'.join(summaries)
     open(dest,'w').write(joined_lines)
 
-def load_data_into_array(top_titles, bot_titles, top_sum, bot_sum):
-    """
-    Reads movie data files and stores them in arrays
-    """
-    return
-
-top_titles = []
 top_summaries = []
-bottom_titles = []
 bottom_summaries = []
-store_movie_data("./movie_titles_top_250.txt")
-load_data_into_array(top_titles, bottom_titles, top_sum, bottom_sum)
+store_movie_data("./movie_summaries_bottom_250.txt")
+#load_data_into_array(top_summaries, bottom_summaries)
---
+++ lda.py
@@ -0,0 +1,32 @@
+def setup_data_arrays(top_t, bottom_t,top_s, bottom_s):
+    """
+    Stores movie titles and summaries into arrays
+    """
+
+    top_movie_titles = open("./movie_titles_top_250.txt", 'r').read()
+    bottom_movie_titles = open("./movie_titles_bottom_250.txt", 'r').read()
+    top_movie_summaries = open("./movie_summaries_top_250.txt", 'r').read()
+    bottom_movie_summaries = open("./movie_summaries_bottom_250.txt", 'r').read()
+
+    """Collects titles"""
+    for line in top_movie_titles.strip().split('\n'):
+        top_t.append(line)
+    for line in bottom_movie_titles.strip().split('\n'):
+        bottom_t.append(line)
+
+    """Collects summaries"""
+    for line in top_movie_summaries.strip().split('\n'):
+        top_s.append(line)
+    for line in bottom_movie_summaries.strip().split('\n'):
+        bottom_s.append(line)
+
+top_titles = []
+bottom_titles = []
+top_summaries = []
+bottom_summaries = []
+setup_data_arrays(top_titles, bottom_titles, top_summaries, bottom_summaries)
+#print(top_titles)
+#print(top_summaries)
+
+#TODO: LDA and K clustering following this guide
+#http://brandonrose.org/clustering#Latent-Dirichlet-Allocation
